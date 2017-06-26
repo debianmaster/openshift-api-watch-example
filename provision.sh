3d26424e912698322ce5f13aeac4bc2489ac0c1f
tetradev1 <9chakri+ds@gmail.com>
2017-06-26 11:57:57 -0700

    first commit

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ oc adm policy add-cluster-role-to-user cluster-admin -z default
 export registry=$(oc get svc docker-registry -n default  -o jsonpath='{.spec.clusterIP}')
 oc new-build https://github.com/debianmaster/openshift-api-watch-example --name=watch-img
 oc new-build https://github.com/debianmaster/simple-scoreboard --name=dash-img
+echo "Waiting on build..... 2 min"
 sleep 120
 oc new-app watch-img --name=watch
 
